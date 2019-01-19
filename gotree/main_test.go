f7fe8dac5eb720151b1624aca5d97e4a86a8a6ea
yaeshimo <hatsushimoyae@gmail.com>
2019-01-19 16:43:47 +0900

    rename

--- gotree/main_test.go
+++ gotree/main_test.go
@@ -2,24 +2,26 @@ package main
 
 import (
 	"bytes"
+	"path/filepath"
 	"testing"
 )
 
 func TestRun(t *testing.T) {
+	const dir = "testdata"
 	tests := []struct {
 		name string
 		opt  *option
 		err  bool
 	}{
-		{name: "basic", opt: &option{root: "t"}},
-		{name: "ignore", opt: &option{root: "t", ignore: "dir3"}},
-		{name: "total", opt: &option{root: "t", total: true}},
-		{name: "version", opt: &option{root: "t", version: true}},
-		{name: "fullpath", opt: &option{root: "t", full: true}},
-		{name: "dirs", opt: &option{root: "t", dirs: true}},
-		{name: "verbose", opt: &option{root: "t", verbose: true}},
+		{name: "basic", opt: &option{root: dir}},
+		{name: "ignore", opt: &option{root: dir, ignore: "dir3"}},
+		{name: "total", opt: &option{root: dir, total: true}},
+		{name: "version", opt: &option{root: dir, version: true}},
+		{name: "fullpath", opt: &option{root: dir, full: true}},
+		{name: "dirs", opt: &option{root: dir, dirs: true}},
+		{name: "verbose", opt: &option{root: dir, verbose: true}},
 
-		{name: "invalid root", opt: &option{root: "t/invalid"}, err: true},
+		{name: "invalid root", opt: &option{root: filepath.Join(dir, "invalid")}, err: true},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
